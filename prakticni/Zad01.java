f873e57b6b8a92999d828ef6529a34d6f64881ee
Gibo2706 <bogdan.markovic2706@gmail.com>
2022-03-11 11:48:44 +0100

    Prakticni zad 11.03.2022

--- prakticni/Zad01.java
+++ prakticni/Zad01.java
@@ -11,19 +11,25 @@ public class Zad01{
         }
     }
     static void saveArrayToFile(String file, Knjiga[] spisak){
-        for(int i = 0 ; i<=spisak.length; i++){
-            Svetovid.out(file).println(spisak[i]);
+        if(spisak != null){
+            for(int i = 0 ; i<=Knjiga.lastIndex(); i++){
+                Svetovid.out(file).println(spisak[i]);
+            }
+            Svetovid.out(file).close();
+            Svetovid.out.println("Uspesno upisano u fajl: " + file);
+        }else{
+            Svetovid.out.println("Spisak je prazan.");
         }
-        Svetovid.out(file).close();
     }
     public static void main(String[] args) {
         String file = Svetovid.in.readLine("Unesite ime fajla: ");
         ucitavanje(file);
         Knjiga[] spisak = Knjiga.ucitajKnjige(file);
         int lastIndex = Knjiga.lastIndex();
-        spisak[lastIndex+1].naslov = Svetovid.in.readLine("Unesite ime knjige: ");
-        spisak[lastIndex+1].ime = Svetovid.in.readLine("Unesite ime pisca: ");
-        spisak[lastIndex+1].godina = Svetovid.in.readInt("Unesite godinu kad je knjiga objavljena: ");
+        String naslov = Svetovid.in.readLine("Unesite ime knjige: ");
+        String ime = Svetovid.in.readLine("Unesite ime pisca: ");
+        int godina = Svetovid.in.readInt("Unesite godinu kad je knjiga objavljena: ");
+        spisak[lastIndex] = new Knjiga(naslov, ime, godina);
         saveArrayToFile("output.txt", spisak);
     }
 }
@@ -44,9 +50,10 @@ class Knjiga{
         Knjiga[] spisak = new Knjiga[MAX_KNJIGA];
         if(Svetovid.testIn(file)){
             while(Svetovid.in(file).hasMore()){
-                spisak[brojac].naslov = Svetovid.in(file).readToken();
-                spisak[brojac].ime = Svetovid.in(file).readToken();
-                spisak[brojac].godina = Svetovid.in(file).readInt();
+                String naslov = Svetovid.in(file).readToken();
+                String ime = Svetovid.in(file).readToken();
+                int godina = Svetovid.in(file).readInt();
+                spisak[brojac] = new Knjiga(naslov, ime, godina);
                 brojac++;
             }
             Svetovid.in(file).close();
